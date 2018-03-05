ab22954e48b550fcf02ce7e3017be29e49dd32c5
sovmestimost <37065544+sovmestimost@users.noreply.github.com>
2018-03-05 15:43:22 +0400

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -12,22 +12,17 @@ document.getElementById('check').onclick = function () {
     var usernum3 = document.getElementById('mynum3').value;
     var out = document.getElementById('out');
     if(
-         usernum === "taron" && usernum1 ==="nersisyan" && usernum2 === "hasmik" && usernum3 === "grigoryan"
+         usernum === "taron" && usernum1 === "nersisyan" && usernum2 === "hasmik" && usernum3 === "grigoryan" ||
+         usernum === "Taron" && usernum1 === "Nersisyan" && usernum2 === "Hasmik" && usernum3 === "Grigoryan" ||
+         usernum === "hasmik" && usernum1 === "grigoryan" && usernum2 === "taron" && usernum3 === "nersisyan" ||
+         usernum === "Hasmik" && usernum1 === "Grigoryan" && usernum2 === "Taron" && usernum3 === "Nersisyan"
 
     ) {
         out.innerHTML = '<p class="name3">100%<p>';
         
 
-    } else if (usernum === "Taron" && usernum1 === "Nersisyan" && usernum2 === "Hasmik" && usernum3 === "Grigoryan"){
-        out.innerHTML = '<p class="name3">100%<p>';
-
-    }else if (usernum2 === "taron" && usernum3 === "nersisyan" && usernum === "hasmik" && usernum1 === "grigoryan"){
-        out.innerHTML = '<p class="name3">100%<p>';
-
-    } else if (usernum2 === "Taron" && usernum3 === "Nersisyan" && usernum === "Hasmik" && usernum1 === "Grigoryan") {
-        out.innerHTML = '<p class="name3">100%<p>';
-
-    }else if (usernum === "taron" && usernum1 === "nersisyan" && usernum2 === "hasmik" && usernum3 === "shahbazyan"){
+    }
+    else if (usernum === "taron" && usernum1 === "nersisyan" && usernum2 === "hasmik" && usernum3 === "shahbazyan"){
         out.innerHTML = '<i class="srtik1">♥</i>';
 
     } else if (usernum2 === "taron" && usernum3 === "nersisyan" && usernum === "hasmik" && usernum1 === "shahbazyan") {
