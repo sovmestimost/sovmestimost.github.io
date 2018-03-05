7d5f5117254ec7ea2700c0edce164b280ae8ae02
sovmestimost <37065544+sovmestimost@users.noreply.github.com>
2018-03-05 12:37:19 +0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,35 @@
+document.getElementById("mynum")
+    .addEventListener("keyup", function (event) {
+        event.preventDefault();
+        if (event.keyCode == 13) {
+            document.getElementById("check").click();
+        }
+    });
+document.getElementById('check').onclick = function () {
+    var usernum = document.getElementById('mynum').value;
+    var usernum1 = document.getElementById('mynum1').value;
+    var usernum2 = document.getElementById('mynum2').value;
+    var usernum3 = document.getElementById('mynum3').value;
+    var out = document.getElementById('out');
+    if(
+         usernum === "taron" && usernum1 ==="nersisyan" && usernum2 === "hasmik" && usernum3 === "grigoryan"
+
+    ) {
+        out.innerHTML = '<i class="srtik1">♥</i>';
+        
+
+    } else if (usernum2 === "taron" && usernum3 === "nersisyan" && usernum === "hasmik" && usernum1 === "grigoryan"){
+        out.innerHTML = '<i class="srtik1">♥</i>';
+
+    } else if (usernum === "taron" && usernum1 === "nersisyan" && usernum2 === "hasmik" && usernum3 === "shahbazyan"){
+        out.innerHTML = '<p class="zang">call me</p>';
+    } else if (usernum2 === "taron" && usernum3 === "nersisyan" && usernum === "hasmik" && usernum1 === "shahbazyan") {
+        out.innerHTML = '<p class="zang">call me</p>';
+    } else    {
+        
+        var random = Math.floor(Math.random() * 98) + 1
+        out.innerHTML = [random] + '%';
+
+    } 
+
+};
