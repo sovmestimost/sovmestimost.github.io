d65e6d091d95ed90f01e841393884b8a22e8622f
sovmestimost <37065544+sovmestimost@users.noreply.github.com>
2018-03-05 18:29:53 +0400

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -15,7 +15,17 @@ document.getElementById('check').onclick = function () {
          usernum === "taron" && usernum1 === "nersisyan" && usernum2 === "hasmik" && usernum3 === "grigoryan" ||
          usernum === "Taron" && usernum1 === "Nersisyan" && usernum2 === "Hasmik" && usernum3 === "Grigoryan" ||
          usernum === "hasmik" && usernum1 === "grigoryan" && usernum2 === "taron" && usernum3 === "nersisyan" ||
-         usernum === "Hasmik" && usernum1 === "Grigoryan" && usernum2 === "Taron" && usernum3 === "Nersisyan"
+         usernum === "Hasmik" && usernum1 === "Grigoryan" && usernum2 === "Taron" && usernum3 === "Nersisyan" ||
+         usernum === "тарон" && usernum1 === "нерсисян" && usernum2 === "асмик" && usernum3 === "григорян" ||
+         usernum === "Тарон" && usernum1 === "Нерсисян" && usernum2 === "Асмик" && usernum3 === "Григорян" ||
+         usernum === "асмик" && usernum1 === "григорян" && usernum2 === "тарон" && usernum3 === "нерсисян" ||
+         usernum === "Асмик" && usernum1 === "Григорян" && usernum2 === "Тарон" && usernum3 === "Нерсисян" ||
+        usernum === "տարոն" && usernum1 === "ներսիսյան" && usernum2 === "հասմիկ" && usernum3 === "գրիգորյան" ||
+        usernum === "Տարոն" && usernum1 === "ներսիսյան" && usernum2 === "Հասմիկ" && usernum3 === "Գրիգորյան" ||
+        usernum === "հասմիկ" && usernum1 === "գրիգորյան" && usernum2 === "taron" && usernum3 === "ներսիսյան" ||
+        usernum === "Հասմիկ" && usernum1 === "Գրիգորյան" && usernum2 === "Taron" && usernum3 === "Ներսիսյան" 
+
+
 
     ) {
         out.innerHTML = '<p class="name3">100%<p>';
